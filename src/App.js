43ec47c591130421d8e9ee2d7b0441878f9724bb
Ciobanu Ioana <ciobanuioana@192-168-0-112.rdsnet.ro>
2020-03-23 00:02:01 +0200

    Day 6

--- src/App.js
+++ src/App.js
@@ -1,176 +1,159 @@
 import React from 'react';
+import fetch from 'axios'
 import logo from './logo.svg';
 import './App.css';
 
 
-class AddForm extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      title: '',
-      description: '',
-      imageUrl: ''
-    }
-
-    this.onClick = this.onClick.bind(this);
-    this.handleTitleChange = this.handleTitleChange.bind(this);
-    this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
-    this.handleImageChange = this.handleImageChange.bind(this);
-  }
-
-  onClick() {
-    const post = {
-      title: this.state.title,
-      description: this.state.description,
-      url: this.state.imageUrl
-    };
-    this.props.onAdd(post);
-  }
-  
-  handleTitleChange(event) {
-    this.setState({
-      title: event.target.value
-    });
-  }
-
-  handleDescriptionChange(event) {
-    this.setState({
-      description: event.target.value
-    });
-  }
-
-  handleImageChange(event) {
-    this.setState({
-      imageUrl: event.target.value
-    });
-  }
-
-  render () {
-    // this.props.onAdd should be called when click on submit
-    return (
-      <div className = 'form'>
-        <div className = 'one'>
-          <input placeholder = 'Title' type = 'text' value = {this.state.title} onChange = {this.handleTitleChange} />
-        </div>
-        <div className = 'two'> 
-          <textarea placeholder = 'Description' type = 'text' value = {this.state.description} onChange = {this.handleDescriptionChange}></textarea>
-        </div>
-        <div className= 'three'>
-          <input placeholder = 'Image Url' type = 'text' value ={this.state.imageUrl} onChange = {this.handleImageChange}></input>
-        </div>
-        <div>
-          <button onClick={this.onClick}>Submit</button>
-        </div>
-      </div>
-
-    );
-  }
-};
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      postsArray: [{
-        title: 'Obraznicul obraznicilor',
-        description: 'Cel mai obraznic dintre pamanteni.',
-        url: 'https://www.animalzoo.ro/wp-content/uploads/2017/05/cu11.jpg'
-      },
-      {
-        title: 'Adormitul adormitilor',
-        description: 'Cel mai adormit din lumea asta mare.',
-        url: 'https://images.megapixl.com/5784/57840756.jpg'
-      },
-      {
-        title: 'Iubibilul iubitilor',
-        description: 'Cel mai iubibil caine din univers.',
-        url: 'https://images.unsplash.com/photo-1551408687-4fa2bd0b683a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'
-      }
-    ]
-    }
+      deckId: '',
+      currentCard: '',
+      cards: [],
+      remaining: 1,
+      sum: 0
 
-    this.onAddPost = this.onAddPost.bind(this);
-    this.handleClickOnDelete = this.handleClickOnDelete.bind(this);
+    };
+
+    this.onShuffle = this.onShuffle.bind(this);
+    this.onDraw = this.onDraw.bind(this);
   }
 
-  onAddPost(post) {
-    /* post should look like
-    {
-      title: 'Titlu',
-      description: 'Descrierere',
-      url: ''
-    }
-    */
-    const copyArray = Object.assign([], this.state.postsArray);
-    copyArray.push(post);
-    this.setState({
-      postsArray: copyArray
+   realValue (valueOfCurrentCard) {
+    
+        if(valueOfCurrentCard === '0') { return 10;}
+          else if (valueOfCurrentCard === 'J') { return 11;}
+            else if (valueOfCurrentCard === 'Q') { return 12;}
+              else if (valueOfCurrentCard === 'K') { return 13;}
+               else if (valueOfCurrentCard === 'A') { return 1;}
+                else {
+                  return  valueOfCurrentCard[0] * 1;
+                }
+      
+    
+  }
 
+  generateObject(data) {
+    const card = data.cards[0];
+    return {
+      image: card.image,
+      code: card.code,
+      value: this.realValue(card.code[0])
+    }
+  }
+
+
+  onShuffle() {
+    console.log('shuffle');
+    const url = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
+    this.setState({
+      deckId: '',
+      currentCard: '',
+      cards: [],
+      remaining: 1
+    });
+
+    const promise = fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    promise.then((response) => {
+      if (response.status === 200) {
+        const deckId = response.data.deck_id;
+        this.setState({
+          deckId: deckId
+        });
+      }
+    });
+  }
+
+  onDraw() {
+    const url ='https://deckofcardsapi.com/api/deck/'+ this.state.deckId +'/draw/?count=1'
+    const promise = fetch(url, {
+      method: 'GET',
+      eaders: {
+        'Content-Type': 'application/json'
+      }
     });
     
-    console.log('a post was added');
-    console.log(post);
-  }
+    promise.then((response) => {
+      console.log('Response:',response);
+      if(response.status === 200) { 
+        const cards= Object.assign([], this.state.cards);
+        const currentCard = this.generateObject(response.data);
+        cards.push(currentCard);
+       
+      
+        // console.log('Show me:', response.data.cards[0].code[0]);
+        // let sum = this.state.sum;
+        // sum = sum + this.realValue(response.data.cards[0].code[0]);
+        // console.log(sum);
+          
+        const remaining = response.data.remaining;
+        
+        this.setState({
+          currentCard: currentCard,
+          cards: cards,
+          remaining: remaining,
+          // sum: sum
 
-  handleClickOnDelete(index) {
-    console.log(index)
-    const newArray = Object.assign([], this.state.postsArray);
-    newArray.splice(index, 1);
-    this.setState({
-      postsArray: newArray
+        }); 
+      } 
+
     });
   }
 
+
+
   render() {
+    const verify = this.state.deckId !== '';
+    const cardsLeft = this.state.remaining !== 0;
+    let sum = 0;
+    this.state.cards.forEach((x) => {
+      sum = sum + x.value;
+    });
+    console.log(this.state.cards);
+    console.log('My sum is:',sum);
     return (
       <div className="App">
-        <h1>DayFive</h1>
-        <AddForm onAdd={this.onAddPost} />
-
+        <h1>Deck of cards</h1>
+        <button onClick={this.onShuffle}>Shuffle a new deck</button>
         <div>
-          <Posts listOfPosts={this.state.postsArray} onDelete={this.handleClickOnDelete}/>
+          {
+            (verify && cardsLeft) ? 
+              <div>
+                <button onClick={this.onDraw}>Draw a card</button>
+                <div> <img src={this.state.currentCard.image} /> </div> 
+              </div> : null
+          }
         </div>
+        <ShowCards allCards={this.state.cards} />
+  
       </div>
     ); 
   }
 }
 
-class Posts extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
-
+class ShowCards extends React.Component {
   render() {
     return (
       <div>
-        {this.props.listOfPosts.map((x, index) => (
-          <Post postInfo={x} index={index} onDelete={this.props.onDelete}/>
-        ))}
+        { 
+          this.props.allCards.map((x) => (
+            console.log(x.image),
+            <img src={x.image}/>
+          ))
+        }
       </div>
-    );
+     
+    )
   }
 }
-
-class Post extends React.Component {
-  render() {
-    return (
-      <div>
-        <h2>{this.props.postInfo.title}</h2>
-        <div>{this.props.postInfo.description}</div>
-        <img src={this.props.postInfo.url} />
-        <div>
-            <button onClick={() => {this.props.onDelete(this.props.index)}}>Delete</button>
-        </div>
-      </div>
-
-    );
-  }
-  
-}
-
-
 
 export default App;
