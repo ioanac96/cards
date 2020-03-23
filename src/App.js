dfaebeecfbbaa7eb37e19389a4078b3e2676ac31
Ciobanu Ioana <ciobanuioana@192-168-0-112.rdsnet.ro>
2020-03-23 20:19:43 +0200

    Day 7: BlackJack

--- src/App.js
+++ src/App.js
@@ -11,24 +11,33 @@ class App extends React.Component {
 
     this.state = {
       deckId: '',
-      currentCard: '',
       cards: [],
-      remaining: 1,
-      sum: 0
-
+      computerCards: [],
+      playerStopped: false,
+      computerStopped: false
     };
 
     this.onShuffle = this.onShuffle.bind(this);
-    this.onDraw = this.onDraw.bind(this);
+    this.onDrawForPlayer = this.onDrawForPlayer.bind(this);
+    this.realValue = this.realValue.bind(this);
+    this.clickStop = this.clickStop.bind(this);
+    this.computerMoves = this.computerMoves.bind(this);
+    this.getCard = this.getCard.bind(this);
+  }
+
+  
+  clickStop(){
+    this.setState({
+      playerStopped: true
+    });
+    this.computerMoves();
   }
 
    realValue (valueOfCurrentCard) {
     
         if(valueOfCurrentCard === '0') { return 10;}
-          else if (valueOfCurrentCard === 'J') { return 11;}
-            else if (valueOfCurrentCard === 'Q') { return 12;}
-              else if (valueOfCurrentCard === 'K') { return 13;}
-               else if (valueOfCurrentCard === 'A') { return 1;}
+          else if (valueOfCurrentCard === 'J' || valueOfCurrentCard === 'Q' || valueOfCurrentCard === 'K') { return 10;}
+               else if (valueOfCurrentCard === 'A') { return 11;}
                 else {
                   return  valueOfCurrentCard[0] * 1;
                 }
@@ -51,9 +60,11 @@ class App extends React.Component {
     const url = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
     this.setState({
       deckId: '',
-      currentCard: '',
       cards: [],
-      remaining: 1
+      computerCards: [],
+      playerStopped: false,
+      computerStopped: false
+
     });
 
     const promise = fetch(url, {
@@ -73,7 +84,7 @@ class App extends React.Component {
     });
   }
 
-  onDraw() {
+  getCard() {
     const url ='https://deckofcardsapi.com/api/deck/'+ this.state.deckId +'/draw/?count=1'
     const promise = fetch(url, {
       method: 'GET',
@@ -81,60 +92,100 @@ class App extends React.Component {
         'Content-Type': 'application/json'
       }
     });
-    
-    promise.then((response) => {
-      console.log('Response:',response);
-      if(response.status === 200) { 
-        const cards= Object.assign([], this.state.cards);
-        const currentCard = this.generateObject(response.data);
-        cards.push(currentCard);
-       
+    return new Promise((resolve, reject) => {
+      promise.then((response) => {
+        if(response.status === 200)
+          resolve(this.generateObject(response.data));
+      });
+    })
+  }
+
+  onDrawForPlayer() {
+    this.getCard().then((currentCard) => {
+   
+      const cards= Object.assign([], this.state.cards);
+      cards.push(currentCard);
       
-        // console.log('Show me:', response.data.cards[0].code[0]);
-        // let sum = this.state.sum;
-        // sum = sum + this.realValue(response.data.cards[0].code[0]);
-        // console.log(sum);
-          
-        const remaining = response.data.remaining;
-        
-        this.setState({
-          currentCard: currentCard,
-          cards: cards,
-          remaining: remaining,
-          // sum: sum
-
-        }); 
-      } 
-
+      this.setState({
+        cards: cards,
+      }); 
     });
   }
+
+  computerMoves() {
+    this.getCard().then((currentCard) => {
+   
+      const cards= Object.assign([], this.state.computerCards);
+      cards.push(currentCard);
+      const sum = this.cardsSum(cards);
+      if (sum > 14) {
+        this.setState({
+          computerStopped: true
+        });
+      }
+      else this.computerMoves();
+      
+      this.setState({
+        computerCards: cards,
+      }); 
+    });
+
+  }
+
+  cardsSum(cardsArray) {
+    let sum = 0;
+    cardsArray.forEach((x) => {
+      sum = sum + x.value;
+   });
+
+   cardsArray.forEach((x) => {
+     if(x.value === 11 && sum > 21)
+       sum = sum - 10;
+   });
+   return sum;
+  }
+
 
 
 
   render() {
     const verify = this.state.deckId !== '';
-    const cardsLeft = this.state.remaining !== 0;
-    let sum = 0;
-    this.state.cards.forEach((x) => {
-      sum = sum + x.value;
-    });
+    const playerSum = this.cardsSum(this.state.cards);
+    const computerSum = this.cardsSum(this.state.computerCards);
+    let result = '';
+    if(this.state.computerStopped && this.state.playerStopped){
+      if(playerSum > computerSum && playerSum <= 21) result = 'Player won!';
+        else if(playerSum > computerSum && playerSum > 21) result = 'Computer won!';
+         else if(playerSum < computerSum && computerSum <= 21) result = 'Computer won!';
+          else if(playerSum < computerSum && computerSum > 21) result = 'Player won!';
+            else if(playerSum === computerSum || (computerSum > 21 && playerSum > 21)) result = 'It is equality!';
+    }
+    
     console.log(this.state.cards);
-    console.log('My sum is:',sum);
+    console.log('My sum is:', playerSum);
     return (
       <div className="App">
-        <h1>Deck of cards</h1>
-        <button onClick={this.onShuffle}>Shuffle a new deck</button>
-        <div>
-          {
-            (verify && cardsLeft) ? 
-              <div>
-                <button onClick={this.onDraw}>Draw a card</button>
-                <div> <img src={this.state.currentCard.image} /> </div> 
-              </div> : null
-          }
-        </div>
+        <h1>BlackJack</h1>
+        <button onClick={this.onShuffle}>Start a new game</button>
+        {
+          (verify) ? 
+            <div>
+              <button onClick={this.onDrawForPlayer} disabled={playerSum > 21 || this.state.playerStopped}>Draw a card</button>
+              <button onClick={this.clickStop} disabled={this.state.playerStopped}>Stop</button>
+    
+            </div> : null
+        }
+        <div>{playerSum}</div>
         <ShowCards allCards={this.state.cards} />
-  
+      
+        {
+          this.state.playerStopped ? 
+          <div>
+            <div>Computer sum: {computerSum}</div>
+            <ShowCards allCards={this.state.computerCards} />
+          </div> : null
+        }
+        <div>{result}</div>
       </div>
     ); 
   }
